e4b306cb7972578fef12d249d3bb7c365d69ac4e
Bodhi <110378320+B0DH1i@users.noreply.github.com>
2024-05-23 04:35:02 +0300

    Update effect.lua

--- effect.lua
+++ effect.lua
@@ -1,370 +1,291 @@
--- AO EFFECT: Game Mechanics for AO Arena Game
+successText = 'Success'
+failedText = "Failed"
+rankList = rankList or {}
+pointsList = pointsList or {}
+members = members or {}
+gameTimeTag = gameTimeTag or ""
+cards = { "Bonus 10", "Bonus 20", "Bonus 30", "Extra Roll", "Lose 10", "Lose 20", "Lose 30", "Swap Points", "Double Points" }
+turnOrder = turnOrder or {}
+currentTurnIndex = currentTurnIndex or 1
 
--- Game grid dimensions
-Width = 40 -- Width of the grid
-Height = 40 -- Height of the grid
-Range = 3 -- The distance for blast effect
-
--- Player energy settings
-MaxEnergy = 100 -- Maximum energy a player can have
-EnergyPerSec = 1 -- Energy gained per second
-
--- Attack settings
-AverageMaxStrengthHitsToKill = 3 -- Average number of hits to eliminate a player
-
--- Initializes default player state
--- @return Table representing player's initial state
-function playerInitState()
-    return {
-        x = math.random(0, Width - 1),
-        y = math.random(0, Height - 1),
-        health = 100,
-        energy = 0,
-        alive = true
-    }
+local function guid()
+    local seed = { 'e', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' }
+    local tb = {}
+    for i = 1, 32 do
+        table.insert(tb, seed[math.random(1, 16)])
+    end
+    local sid = table.concat(tb)
+    return string.format('%s-%s-%s-%s-%s',
+            string.sub(sid, 1, 8),
+            string.sub(sid, 9, 12),
+            string.sub(sid, 13, 16),
+            string.sub(sid, 17, 20),
+            string.sub(sid, 21, 32)
+    )
 end
 
--- Function to incrementally increase player's energy
--- Called periodically to update player energy
-function onTick()
-    if GameMode ~= "Playing" then return end  -- Only active during "Playing" state
+local function getDiceNumber()
+    return math.random(1, 6)
+end
 
-    if LastTick == nil then LastTick = Now end
+local function getDiceText(num)
+    return num > 1 and successText or failedText
+end
 
-    local Elapsed = Now - LastTick
-    if Elapsed >= 1000 then  -- Actions performed every second
-        for player, state in pairs(Players) do
-            if state.alive then
-                local newEnergy = math.min(MaxEnergy, state.energy + math.floor((Elapsed / 1000) * EnergyPerSec))
-                state.energy = newEnergy
-            end
-        end
-        LastTick = Now
+local function getCard()
+    return cards[math.random(1, #cards)]
+end
+
+local function getMembers()
+    local memberList = {}
+    for id, _ in pairs(members) do
+        table.insert(memberList, id)
+    end
+    return memberList
+end
+
+local function addMember(id)
+    if not members[id] then
+        members[id] = true
+        table.insert(turnOrder, id)
     end
 end
 
--- Handles player movement
--- @param msg: Message request sent by player with movement direction and player info
-function move(msg)
-    local playerToMove = msg.From
-    local direction = msg.Tags.Direction
+local function getNextTurn()
+    currentTurnIndex = currentTurnIndex + 1
+    if currentTurnIndex > #turnOrder then
+        currentTurnIndex = 1
+    end
+    return turnOrder[currentTurnIndex]
+end
 
-    local directionMap = {
-        Up = {x = 0, y = -1}, Down = {x = 0, y = 1},
-        Left = {x = -1, y = 0}, Right = {x = 1, y = 0},
-        UpRight = {x = 1, y = -1}, UpLeft = {x = -1, y = -1},
-        DownRight = {x = 1, y = 1}, DownLeft = {x = -1, y = 1}
-    }
+local function getCurrentTurn()
+    return turnOrder[currentTurnIndex]
+end
 
-    -- Calculate and update new coordinates
-    if directionMap[direction] and Players[playerToMove].alive then
-        local newX = (Players[playerToMove].x + directionMap[direction].x) % Width
-        local newY = (Players[playerToMove].y + directionMap[direction].y) % Height
+local function joinStatistic(id)
+    addMember(id)
+end
 
-        -- Updates player coordinates while checking for grid boundaries
-        Players[playerToMove].x = newX
-        Players[playerToMove].y = newY
+local function getPersonPoints(id)
+    return pointsList[id] or 0
+end
 
-        announce("Player-Moved", playerToMove .. " moved to " .. newX .. "," .. newY .. ".")
+local function sortRankList()
+    table.sort(rankList, function(a, b)
+        return a.points > b.points
+    end)
+end
+
+local function getGameTimeTag()
+    local currentTime = os.date("*t", os.time())
+    return currentTime.year .. '-' .. currentTime.month
+end
+
+local function checkRankExpire()
+    local currentTag = getGameTimeTag()
+    if gameTimeTag ~= currentTag then
+        for i = 1, 10 do
+            if rankList[i] and rankList[i].pid then
+                ao.send({ Target = ao.id, Action = "Transfer", Recipient = rankList[i].pid, Quantity = tostring(100 - (i - 1) * 10) })
+            end
+        end
+        rankList = {}
+        gameTimeTag = currentTag
+    end
+end
+
+local function log(message)
+    print("[LOG] " .. message)
+end
+
+local function calculatePoints(diceNumber, cardText, points)
+    if not diceNumber or not cardText then
+        log("Invalid dice number or card text")
+        return points
+    end
+    if (diceNumber > 1) then
+        points = points + diceNumber
+        if cardText == "Bonus 10" then
+            points = points + 10
+        elseif cardText == "Bonus 20" then
+            points = points + 20
+        elseif cardText == "Bonus 30" then
+            points = points + 30
+        elseif cardText == "Lose 10" then
+            points = points - 10
+        elseif cardText == "Lose 20" then
+            points = points - 20
+        elseif cardText == "Lose 30" then
+            points = points - 30
+        elseif cardText == "Extra Roll" then
+            points = points + diceNumber
+        elseif cardText == "Swap Points" then
+            local swapId = getMembers()[math.random(1, #getMembers())]
+            points, pointsList[swapId] = pointsList[swapId], points
+        elseif cardText == "Double Points" then
+            points = points * 2
+        end
     else
-        ao.send({Target = playerToMove, Action = "Move-Failed", Reason = "Invalid direction or player is dead."})
+        points = 0
     end
-    onTick()  -- Optional: Update energy each move
-    ao.send({Target = playerToMove, Action = "Tick"})
+    return points
 end
 
--- Handles player attacks
--- @param msg: Message request sent by player with attack info and player state
-function attack(msg)
-    local player = msg.From
-    local attackEnergy = math.abs(tonumber(msg.Tags.AttackEnergy))
-
-    if Players[player] == nil or not Players[player].alive then
-        ao.send({Target = player, Action = "Attack-Failed", Reason = "Player does not exist or is dead."})
-        return
-    end
-
-    -- Get player coordinates
-    local x = Players[player].x
-    local y = Players[player].y
-
-    -- Check if player has enough energy to attack
-    if Players[player].energy < attackEnergy then
-        ao.send({Target = player, Action = "Attack-Failed", Reason = "Not enough energy."})
-        return
-    end
-
-    -- Update player energy and calculate damage
-    Players[player].energy = Players[player].energy - attackEnergy
-    local damage = math.floor((math.random() * 2 * attackEnergy) * (1 / AverageMaxStrengthHitsToKill))
-
-    announce("Attack", player .. " has launched a " .. damage .. " damage attack from " .. x .. "," .. y .. "!")
-
-    -- Check if any player is within range and update their status
-    for target, state in pairs(Players) do
-        if target ~= player and state.alive and inRange(x, y, state.x, state.y, Range) then
-            local newHealth = state.health - damage
-            if newHealth <= 0 then
-                eliminatePlayer(target, player)
-            else
-                Players[target].health = newHealth
-                ao.send({Target = target, Action = "Hit", Damage = tostring(damage), Health = tostring(newHealth)})
-                ao.send({Target = player, Action = "Successful-Hit", Recipient = target, Damage = tostring(damage), Health = tostring(newHealth)})
-            end
-        end
-    end
-    ao.send({Target = player, Action = "Tick"})
-end
-
--- Helper function to check if a target is within range
--- @param x1, y1: Coordinates of the attacker
--- @param x2, y2: Coordinates of the potential target
--- @param range: Attack range
--- @return Boolean indicating if the target is within range
-function inRange(x1, y1, x2, y2, range)
-    return math.abs(x1 - x2) <= range and math.abs(y1 - y2) <= range
-end
-
--- HANDLERS: Game state management for AO-Effect
-
--- Handler for player movement
-Handlers.add("PlayerMove", Handlers.utils.hasMatchingTag("Action", "PlayerMove"), move)
-
--- Handler for player attacks
-Handlers.add("PlayerAttack", Handlers.utils.hasMatchingTag("Action", "PlayerAttack"), attack)
-
--- Function to announce game events
--- @param event: Event name
--- @param description: Description of the event
-function announce(event, description)
-    for _, address in pairs(Listeners) do
-        ao.send({
-            Target = address,
-            Action = "Announcement",
-            Event = event,
-            Data = description
-        })
-    end
-    print("Announcement: " .. event .. " - " .. description)
-end
-
--- Function to eliminate a player from the game
--- @param eliminated: Player to be eliminated
--- @param eliminator: Player causing the elimination
-function eliminatePlayer(eliminated, eliminator)
-    Players[eliminated].alive = false
-    Players[eliminator].energy = math.min(MaxEnergy, Players[eliminator].energy + 10)  -- Reward eliminator with energy
-
-    sendReward(eliminator, PaymentQty, "Eliminated-Player")
-    Waiting[eliminated] = false
-
-    ao.send({
-        Target = eliminated,
-        Action = "Eliminated",
-        Eliminator = eliminator
-    })
-
-    announce("Player-Eliminated", eliminated .. " was eliminated by " .. eliminator .. "!")
-
-    local playerCount = 0
-    for _, state in pairs(Players) do
-        if state.alive then
-            playerCount = playerCount + 1
-        end
-    end
-    print("Eliminating player: " .. eliminated .. " by: " .. eliminator) -- Useful for tracking eliminations
-
-    if playerCount < MinimumPlayers then
-        endGame()
-    end
-end
-
--- Function to send a reward to a player
--- @param recipient: The player receiving the reward
--- @param qty: The quantity of the reward
--- @param reason: The reason for the reward
-function sendReward(recipient, qty, reason)
-    if type(qty) ~= "number" then
-      qty = tonumber(qty)
-    end
-    ao.send({
-        Target = PaymentToken,
-        Action = "Transfer",
-        Quantity = tostring(qty),
-        Recipient = recipient,
-        Reason = reason
-    })
-    print("Sent Reward: " .. tostring(qty) .. " tokens to " .. recipient .. " for " .. reason)
-end
-
--- Function to end the current game and start a new one
-function endGame()
-    print("Game Over")
-
-    Winners = 0
-
-    for player, state in pairs(Players) do
-        if state.alive then
-            Winners = Winners + 1
-        end
-    end
-
-    Winnings = tonumber(BonusQty) / Winners
-
-    for player, state in pairs(Players) do
-        if state.alive then
-            sendReward(player, Winnings + tonumber(PaymentQty), "Win")
-            Waiting[player] = false
-        end
-    end
-
-    Players = {}
-    announce("Game-Ended", "Congratulations! The game has ended. Remaining players at conclusion: " .. Winners .. ".")
-    startWaitingPeriod()
-end
-
--- Function to start the waiting period for players to join
-function startWaitingPeriod()
-    GameMode = "Waiting"
-    StateChangeTime = Now + WaitTime
-    announce("Started-Waiting-Period", "The game is about to begin! Send your token to take part.")
-    print('Starting Waiting Period')
-end
-
--- Handler for game state transitions based on cron messages
 Handlers.add(
-    "Game-State-Timers",
+    "HandlerGetRank",
+    Handlers.utils.hasMatchingTag("Action", "GetRank"),
     function(Msg)
-        return "continue"
-    end,
-    function(Msg)
-        Now = Msg.Timestamp
-        if GameMode == "Not-Started" then
-            startWaitingPeriod()
-        elseif GameMode == "Waiting" then
-            if Now > StateChangeTime then
-                startGamePeriod()
-            end
-        elseif GameMode == "Playing" then
-            if onTick and type(onTick) == "function" then
-                onTick()
-            end
-            if Now > StateChangeTime then
-                endGame()
-            end
+        log("HandlerGetRank called")
+        if #rankList == 0 then
+            ao.send({
+                Target = Msg.From,
+                Action = "RankList",
+                Data = "rankList : No Any Person"
+            })
+            return
         end
-    end
-)
 
--- Handler for player registration
-Handlers.add(
-    "Register",
-    Handlers.utils.hasMatchingTag("Action", "Register"),
-    function(Msg)
-        if Msg.Mode ~= "Listen" and Waiting[Msg.From] == nil then
-            Waiting[Msg.From] = false
+        checkRankExpire()
+        local page = tonumber(Msg.Data)
+        if (page == nil) then
+            page = 1
         end
-        removeListener(Msg.From)
-        table.insert(Listeners, Msg.From)
+        local startPos = (page - 1) * 10 + 1
+        if (startPos > #rankList) then
+            startPos = 1
+        end
+        local endPos = startPos + 10
+        local maxPos = math.min(endPos, #rankList)
+        local retText = ''
+        for i = startPos, maxPos do
+            retText = retText .. 'Rank ' .. i .. " : " .. rankList[i].name .. " " .. rankList[i].points
+            if startPos ~= maxPos then
+                retText = retText .. '\n'
+            end
+        end
         ao.send({
             Target = Msg.From,
-            Action = "Registered"
+            Action = "RankList",
+            Data = retText
         })
-        announce("New Player Registered", Msg.From .. " has joined in waiting.")
     end
 )
 
--- Handler for player unregistration
 Handlers.add(
-    "Unregister",
-    Handlers.utils.hasMatchingTag("Action", "Unregister"),
+    "HandlerGetPoints",
+    Handlers.utils.hasMatchingTag("Action", "GetPoints"),
     function(Msg)
-        removeListener(Msg.From)
+        log("HandlerGetPoints called")
+        local text = getPersonPoints(Msg.From)
+        log(text .. " Points")
         ao.send({
             Target = Msg.From,
-            Action = "Unregistered"
+            Action = "CurrentPoints",
+            Data = text .. ""
         })
     end
 )
 
--- Function to remove a listener from the listeners' list
--- @param listener: The listener to be removed
-function removeListener(listener)
-    local idx = 0
-    for i, v in ipairs(Listeners) do
-        if v == listener then
-            idx = i
-            break
+Handlers.add(
+    "HandlerFinishPoints",
+    Handlers.utils.hasMatchingTag("Action", "FinishPoints"),
+    function(Msg)
+        log("HandlerFinishPoints called")
+        checkRankExpire()
+        local uuid = guid()
+        table.insert(rankList, {
+            points = pointsList[Msg.From],
+            name = Msg.Data,
+            uuid = uuid,
+            pid = Msg.From
+        })
+        sortRankList()
+        pointsList[Msg.From] = 0
+        local current = "Unknown"
+        for index, obj in pairs(rankList) do
+            if obj.uuid == uuid then
+                current = index
+                break
+            end
         end
-    end
-    if idx > 0 then
-        table.remove(Listeners, idx)
-    end
-end
 
--- Function to handle player deposits and update their status
-Handlers.add(
-    "Transfer",
-    function(Msg)
-        return
-            Msg.Action == "Credit-Notice" and
-            Msg.From == PaymentToken and
-            tonumber(Msg.Quantity) >= tonumber(PaymentQty) and "continue"
-    end,
-    function(Msg)
-        Waiting[Msg.Sender] = true
-        ao.send({
-            Target = Msg.Sender,
-            Action = "Payment-Received"
-        })
-        announce("Player-Ready", Msg.Sender .. " is ready to play!")
-    end
-)
-
--- Function to retrieve the current game state
-Handlers.add(
-    "GetGameState",
-    Handlers.utils.hasMatchingTag("Action", "GetGameState"),
-    function(Msg)
-        local json = require("json")
-        local TimeRemaining = StateChangeTime - Now
-        local GameState = json.encode({
-            GameMode = GameMode,
-            TimeRemaining = TimeRemaining,
-            Players = Players,
-        })
         ao.send({
             Target = Msg.From,
-            Action = "GameState",
-            Data = GameState})
+            Action = "FinishPointsResult",
+            Data = "Hey! You ranked " .. current
+        })
     end
 )
 
--- Function to alert users regarding the time remaining in each game state
 Handlers.add(
-    "AnnounceTick",
-    Handlers.utils.hasMatchingTag("Action", "Tick"),
+    "HandlerRollDiceAndDrawCard",
+    Handlers.utils.hasMatchingTag("Action", "RollDiceAndDrawCard"),
     function(Msg)
-        local TimeRemaining = StateChangeTime - Now
-        if GameMode == "Waiting" then
-            announce("Tick", "The game will start in " .. (TimeRemaining / 1000) .. " seconds.")
-        elseif GameMode == "Playing" then
-            announce("Tick", "The game will end in " .. (TimeRemaining / 1000) .. " seconds.")
+        log("HandlerRollDiceAndDrawCard called")
+        
+        local currentTurn = getCurrentTurn()
+        if Msg.From ~= currentTurn then
+            ao.send({
+                Target = Msg.From,
+                Action = "Error",
+                Data = "It's not your turn!"
+            })
+            return
         end
+        
+        local diceNumber = getDiceNumber()
+        local diceText = getDiceText(diceNumber)
+        local cardText = getCard()
+        
+        local points = getPersonPoints(Msg.From)
+        points = calculatePoints(diceNumber, cardText, points)
+        
+        pointsList[Msg.From] = points
+        joinStatistic(Msg.From)
+        
+        ao.send({
+            Target = Msg.From,
+            Action = "RollDiceAndDrawCardResult",
+            Data = "Dice: " .. diceText .. ", Card: " .. cardText .. ", Total Points: " .. points
+        })
+        
+        local nextTurn = getNextTurn()
+        ao.send({
+            Target = nextTurn,
+            Action = "YourTurn",
+            Data = "It's your turn now!"
+        })
     end
 )
 
--- Function to send tokens to players with no balance upon request
 Handlers.add(
-    "RequestTokens",
-    Handlers.utils.hasMatchingTag("Action", "RequestTokens"),
+    "HandlerJoinGame",
+    Handlers.utils.hasMatchingTag("Action", "JoinGame"),
     function(Msg)
-        print("Transferring Tokens: " .. tostring(math.floor(10000 * UNIT)))
+        log("HandlerJoinGame called")
+        addMember(Msg.From)
         ao.send({
-            Target = ao.id,
-            Action = "Transfer",
-            Quantity = tostring(math.floor(10000 * UNIT)),
-            Recipient = Msg.From,
+            Target = Msg.From,
+            Action = "JoinGameResult",
+            Data = "You have joined the game!"
+        })
+    end
+)
+
+Handlers.add(
+    "HandlerGetMembers",
+    Handlers.utils.hasMatchingTag("Action", "GetMembers"),
+    function(Msg)
+        local membersList = getMembers()
+        local retText = 'Members:\n'
+        for i, member in ipairs(membersList) do
+            retText = retText .. i .. ": " .. member .. "\n"
+        end
+        ao.send({
+            Target = Msg.From,
+            Action = "MembersList",
+            Data = retText
         })
     end
 )
